4b865a8e81adbc61d6d342127513892da0e07745
Rebecca Dorn <beccadorn@Rebeccas-MacBook-Pro-4.local>
2020-01-20 16:18:52 -0800

    working but runs really slowly

--- A1/ngrams.py
+++ A1/ngrams.py
@@ -2,6 +2,7 @@
 # Assignment 1: Language Modeling and Smoothing
 # Rebecca Dorn, [other names]
 # cruzid: radorn, [other cruzid]
+import pandas as pd
 
 def main():
 	# Get the training data
@@ -15,7 +16,9 @@ def main():
 	unigram_count_vec = ngram_model(train,1) # Get probability distribution for unigram
 	bigram_count_vec = ngram_model(train,2) # Get probability distribution for bigram
 	trigram_count_vec = ngram_model(train,3) # Get probability distribution for trigram
-	#print("Trigram count vector sample: ", trigram_count_vec[:5])
+	#print("Unigram count vector sample: ", unigram_count_vec)
+	#print("Trigram count vector sample: ", trigram_count_vec)
+	print("Calculated counts for unigram, bigram and trigram")
 
 	# Get the dev data
 	dev = [] # Initialize empty list for development
@@ -34,7 +37,7 @@ def main():
 	print("Sample trigram predictions: ",trigram_predictions[:10])
 
 	yhat_interpolated = interpolate(dev, unigram_count_vec, bigram_count_vec, trigram_count_vec, .33, .33, .34)
-	#print("Calculated interpolated data", yhat_interpolated[0:5])
+	print("Calculated interpolated data", yhat_interpolated[0:5])
 
 # Get list of words separated by spaces
 def get_tokens(sentence): # Return a list of normalized words
@@ -46,24 +49,26 @@ def get_tokens(sentence): # Return a list of normalized words
 # Extract dictionary of n-gram vocabulary and counts of those n-grams
 def ngram_model(train, n):
 	ngram_corpus = {} # Initialize our dictionary as empty
-	stop_count = 0 # counts the number of lines, which is the number of stop tokens
 	for instance in train: # For each sentance in train
-		stop_count += 1
 		tokens = get_tokens(instance) # split sentences into words
+		tokens.append('<STOP>')	# append STOP to the end of each sentence
 		for i in range(0, len(tokens)): # For each word in the sentence
 			w = tokens[i:i+n]
-			if(w == []): # out of bound slices return [], so ignore
+			if(len(w) < n): # out of bound slices, ignore
 			   break
-			word = tuple(w)
-			#print(word)
+			if n == 1:
+				word = w[0]
+				#print(word)
+			if n > 1:
+				word = tuple(w)
 			if word not in ngram_corpus.keys(): # if this is a new unigram
 				ngram_corpus[word] = 1 # Initialize it's sightings to 1
 			else:
 				ngram_corpus[word] += 1 # Increment our counter by 1
 	print("Counted n-grams in building for grams length ",n)
-	#print( "Stop count is", stop_count)
+	#print( "Stop count is", ngram_corpus['<STOP>'])
 	# Create new dictionary with frequent unigrams, mapping rare unigrams to 'UNK'
-	freq_corpus = {'UNK':0, 'STOP':stop_count} # Initialize the number of rare words to 0
+	freq_corpus = {'UNK':0} # Initialize the number of rare words to 0
 	for ngram in ngram_corpus.keys(): # Go through each word and it's count
 		if ngram_corpus[ngram] < 3: # If it was not sighted enough
 			freq_corpus['UNK'] += 1 # Don't add to new dictionary, increment 'UNK' counter
@@ -81,23 +86,27 @@ def ngram_predict(vocab,test,n):
 		product = 1 # Initialize product as 1 b/c won't affect multiplication
 		for i in range(0,len(tokens)-n-1): # go through ngrams in this test sentence
 			w = tokens[i:i+n]
-			word = tuple(w)
-			if word in vocab.keys(): # if this word is in our dictionary
+			if(len(w) < n): # out of bound slices, ignore
+			   break
+			if n == 1:
+				word = w[0]
+			if n > 1:
+				word = tuple(w)
+			if word in vocab: # if this word is in our dictionary
 				if n == 1:
 					count = float(vocab[word]) / len(vocab) # p(uni_i) = c(uni_i in train)/c(uni's in vocab)
 				elif n == 2:
-					# get context
+					first, second = word
 					context = 0
-					for bigram in vocab.keys():
-						if bigram[0] == word[0]:
-							context += 1
+					for item in vocab:
+						if item[0] == first:
+							context += vocab[item]
 					count = float(vocab[word]) / context
 				elif n == 3:
-					# get context
 					context = 0
 					for trigram in vocab.keys():
 						if trigram[:2] == word[:2]:
-							context += 1
+							context += vocab[trigram]
 					count = float(vocab[word]) / context
 			else: # else, map this rare word to 'UNK'
 				count = float(vocab['UNK']) / len(vocab) # p('UNK') = c('UNK' in train)/c(uni's in vocab)
@@ -125,11 +134,11 @@ def interpolate(test, unigram, bigram, trigram, lamb_1, lamb_2, lamb_3):
 				theta_uni = float(lamb_1) * ngram_predict(unigram,[instance],1)[0]
 				theta_bi = float(lamb_2) * ngram_predict(bigram,[instance],2)[0]
 				theta_tri = float(lamb_3) * ngram_predict(trigram,[instance],3)[0]
-				#if init == 2:
-					#print("Unigram alone prediction: ", ngram_predict(unigram,[instance],1)[0])
-					#print("Bigram alone prediction: ", ngram_predict(bigram,[instance],2)[0])
-					#print("Trigram alone prediction: ", ngram_predict(trigram,[instance],3)[0])
-					#init += 1
+				if init == 2:
+					print("Unigram alone prediction: ", ngram_predict(unigram,[instance],1)[0])
+					print("Bigram alone prediction: ", ngram_predict(bigram,[instance],2)[0])
+					print("Trigram alone prediction: ", ngram_predict(trigram,[instance],3)[0])
+					init += 1
 		theta_smoothed = theta_uni + theta_bi + theta_tri
 		yhat_interpolated.append(theta_smoothed)
 	return yhat_interpolated
